57e898fb0a52121cb3a52ac4b2f17a132a52d4e9
Claudia Troeger <claudiatroeger2410@yahoo.de>
2022-08-09 15:38:38 +0100

    added show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
     @items = Item.all
+  end
+
+  def show; end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
