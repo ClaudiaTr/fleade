2cc3163f35a1f32aec927d86a092bdbfc19ded83
Claudia Troeger <claudiatroeger2410@yahoo.de>
2022-08-09 16:30:38 +0100

    added rest of CRUD actions for items_controller and their views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,9 +7,40 @@ class ItemsController < ApplicationController
 
   def show; end
 
+  def edit; end
+
+  def update
+    @item.update(item_params)
+    redirect_to item_path(@item)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    @item.user = current_user
+    if @item.save
+      redirect_to item_path(@item)
+   else
+      render :new
+   end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path
+  end
+
   private
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :location, :credit, :image)
   end
 end
